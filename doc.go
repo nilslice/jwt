f217873cbef62bcac24d551864c48281cbc34037
Steve Manuel <stevenhmanuel@gmail.com>
2015-12-25 08:55:34 -0800

    adding initial package files

---
+++ doc.go
@@ -0,0 +1,38 @@
+// package jwt provides methods to create and check JSON Web Tokens. It only implements HMAC 256 encryption and has a very small footprint, ideal for simple usage when authorizing clients
+/*
+
+	package main
+
+	import (
+		auth "github.com/nilslice/jwt"
+		"fmt"
+		"net/http"
+		"strings"
+	)
+
+	func main() {
+		http.HandleFunc("/auth/new", func(res http.ResponseWriter, req *http.Request) {
+			token, err := auth.New()
+			if err != nil {
+				http.Error(res, "Error", 500)
+				return
+			}
+			res.Header().Add("Authorization", "Bearer "+token)
+
+			res.WriteHeader(http.StatusOK)
+		})
+
+		http.HandleFunc("/auth", func(res http.ResponseWriter, req *http.Request) {
+			userToken := strings.Split(req.Header.Get("Authorization"), " ")[1]
+
+			if auth.Passes(userToken) {
+				fmt.Println("ok")
+			} else {
+				fmt.Println("no")
+			}
+		})
+
+		http.ListenAndServe(":8080", nil)
+	}
+*/
+package jwt
---
+++ jwt.go
@@ -0,0 +1,174 @@
+package jwt
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+)
+
+var (
+	privateKey       = []byte("")
+	defaultHeader    = header{Typ: "JWT", Alg: "HS256"}
+	registeredClaims = []string{"iss", "sub", "aud", "exp", "nbf", "iat", "jti"}
+	defaultPayload   = make(payload)
+)
+
+func init() {
+	for _, c := range registeredClaims {
+		defaultPayload[c] = nil
+	}
+}
+
+type header struct {
+	Typ string `json:"typ"`
+	Alg string `json:"alg"`
+}
+
+type payload map[string]interface{}
+
+type encoded struct {
+	token string
+}
+type decoded struct {
+	header  string
+	payload string
+}
+
+type signedDecoded struct {
+	decoded
+	signature string
+}
+
+func newEncoded() (encoded, error) {
+	header, err := json.Marshal(defaultHeader)
+	if err != nil {
+		return encoded{}, err
+	}
+
+	payload, err := json.Marshal(defaultPayload)
+	if err != nil {
+		return encoded{}, err
+	}
+
+	d := decoded{header: string(header), payload: string(payload)}
+
+	d.encodeInternal()
+	signed, err := d.sign()
+	if err != nil {
+		return encoded{}, err
+	}
+
+	token := signed.token()
+	e := encoded{token: token}
+	return e, nil
+}
+
+func newDecoded(token string) (decoded, error) {
+	e := encoded{token: token}
+	d, err := e.parseToken()
+	if err != nil {
+		return d, nil
+	}
+
+	return d, nil
+}
+
+func encodeToString(src []byte) string {
+	return base64.RawURLEncoding.EncodeToString(src)
+}
+
+func (d decoded) getHeader() []byte {
+	return []byte(d.header)
+}
+
+func (d decoded) getPayload() []byte {
+	return []byte(d.payload)
+}
+
+func (sd signedDecoded) getSignature() []byte {
+	return []byte(sd.signature)
+}
+
+func (d *decoded) encodeInternal() {
+	d.header = encodeToString(d.getHeader())
+	d.payload = encodeToString(d.getPayload())
+}
+
+func (d decoded) dot(internals ...string) string {
+	return strings.Join(internals, ".")
+}
+
+func (d *decoded) sign() (signedDecoded, error) {
+	if d.header == "" || d.payload == "" {
+		return signedDecoded{}, errors.New("Missing header or payload on Decoded")
+	}
+
+	unsigned := d.dot(d.header, d.payload)
+
+	hash := hmac.New(sha256.New, privateKey)
+	_, err := hash.Write([]byte(unsigned))
+	if err != nil {
+		return signedDecoded{}, err
+	}
+
+	signed := signedDecoded{decoded: *d}
+	signed.signature = encodeToString(hash.Sum(nil))
+
+	return signed, nil
+}
+
+func (sd signedDecoded) token() string {
+	return fmt.Sprintf("%s.%s.%s", sd.getHeader(), sd.getPayload(), sd.getSignature())
+}
+
+func (sd signedDecoded) verify(enc encoded) bool {
+	if sd.token() == enc.token {
+		return true
+	}
+	return false
+}
+
+func (e encoded) parseToken() (decoded, error) {
+	parts := strings.Split(e.token, ".")
+	if len(parts) != 3 {
+		return decoded{}, errors.New("Error: incorrect # of results from string parsing")
+	}
+
+	d := decoded{
+		header:  parts[0],
+		payload: parts[1],
+	}
+
+	return d, nil
+}
+
+// New returns a token (string) and error. The token is a fully qualified JWT to be sent to a client via HTTP Header or other method. Error returned will be from the newEncoded unexported function.
+func New() (string, error) {
+	enc, err := newEncoded()
+	if err != nil {
+		return "", err
+	}
+
+	return enc.token, nil
+}
+
+// Passes returns a bool indicating whether a token (string) provided has been signed by our server. If true, the client is authenticated and may proceed.
+func Passes(token string) bool {
+	dec, err := newDecoded(token)
+	if err != nil {
+		// may want to log some error here so we have visibility
+		// intentionally simplifying return type to bool for ease
+		// of use in API. Caller should only do `if auth.Passes(str) {}`
+		return false
+	}
+	signed, err := dec.sign()
+	if err != nil {
+		return false
+	}
+
+	return signed.verify(encoded{token: token})
+}
