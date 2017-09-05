eb5cefa7c647aa4209a9e4aadb6a214c253f3989
Steve Manuel <nilslice@gmail.com>
2017-09-05 10:15:57 -0600

    add tests

---
+++ jwt_test.go
@@ -0,0 +1,67 @@
+package jwt_test
+
+import (
+	"log"
+	"testing"
+
+	"github.com/nilslice/jwt"
+)
+
+func TestGrantPasses(t *testing.T) {
+	claims := map[string]interface{}{
+		"testID": 1,
+		"name":   "GrantPasses",
+	}
+
+	grant, err := jwt.New(claims)
+	if err != nil {
+		t.Log(claims, grant, err)
+		t.Fail()
+	}
+
+	if !jwt.Passes(grant) {
+		t.Fail()
+	}
+}
+
+func TestGrantFails(t *testing.T) {
+	claims := map[string]interface{}{
+		"testID": 2,
+		"name":   "GrantFails",
+	}
+
+	grant, err := jwt.New(claims)
+	if err != nil {
+		t.Log(claims, grant, err)
+		t.Fail()
+	}
+
+	jwt.Secret([]byte("NotPreviousSecret"))
+
+	if jwt.Passes(grant) {
+		t.Fail()
+	}
+}
+
+const email = "hello@nilslice.xyz"
+const authLevel = 7
+
+func TestGetClaims(t *testing.T) {
+	claims := map[string]interface{}{
+		"email":      email,
+		"auth_level": authLevel,
+	}
+
+	token, err := jwt.New(claims)
+	if err != nil {
+		log.Println(err)
+		t.Fail()
+	}
+
+	claims = jwt.GetClaims(token)
+	if claims["email"].(string) != email ||
+		claims["auth_level"].(float64) != authLevel {
+		t.Fail()
+	}
+
+}
